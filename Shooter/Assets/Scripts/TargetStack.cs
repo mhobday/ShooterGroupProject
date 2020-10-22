bc84054778d317056c3b240f953db4fd909c1b8a
hawk588 <gdhawk588@gmail.com>
2020-10-22 16:44:46 -0500

    Added Target Stack

---
+++ Shooter/Assets/Scripts/TargetStack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetStack : Target
+{
+    // Start is called before the first frame update
+    public float impactForce;
+
+    Rigidbody targetRigidbody;
+    void Start()
+    {
+        targetRigidbody = target.GetComponent<Rigidbody>();
+    }
+
+
+    public override void Process(RaycastHit hit)
+    {
+        targetRigidbody.AddForce(-hit.normal * impactForce);
+
+        effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+    }
+}
